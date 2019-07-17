2c8fac52d3e5bd63a91ddf64afade9bbbdd20aa0
James Eby <jeby@jamess-mbp.attlocal.net>
2019-07-17 19:14:31 -0400

    small sample data used[3]

--- src/app/@core/data/smart-table.ts
+++ src/app/@core/data/smart-table.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   })
 export class SmartTableData {
 
-    CAData:Object[] = [
+    CAData: Object[] = [
         {
             LABORATORIO: 'Naprod',
             ARTICULO: 10118212,
