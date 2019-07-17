e098c169bcf1685db74a60e47cd29fd910bb2b8c
James Eby <jeby@jamess-mbp.attlocal.net>
2019-07-17 16:05:37 -0400

    ng2-smart-table in CA tab

--- src/app/pages/cruz-azul/cruz-azul.component.ts
+++ src/app/pages/cruz-azul/cruz-azul.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ng2SmartTableModule } from 'ng2-smart-table';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 @Component({
   selector: 'ngx-cruz-azul',
@@ -7,6 +7,47 @@ import { ng2SmartTableModule } from 'ng2-smart-table';
   styleUrls: ['./cruz-azul.component.scss'],
 })
 export class CruzAzulComponent implements OnInit {
+
+  settings = {
+    columns: {
+      id: {
+        title: 'ID'
+      },
+      name: {
+        title: 'Full Name'
+      },
+      username: {
+        title: 'User Name'
+      },
+      email: {
+        title: 'Email'
+      }
+    },
+  };
+
+  data = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz"
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      username: "Antonette",
+      email: "Shanna@melissa.tv"
+    },
+    
+    // ... list of items
+    
+    {
+      id: 11,
+      name: "Nicholas DuBuque",
+      username: "Nicholas.Stanton",
+      email: "Rey.Padberg@rosamond.biz"
+    },
+  ];
 
   constructor() { }
 
